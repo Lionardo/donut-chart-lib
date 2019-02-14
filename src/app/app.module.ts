5835ff8fb6a099d77c6b5b384754d6d07c994684
lionardo <m.lionardo@gmail.com>
2019-02-14 18:47:20 +0100

    update package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 // import { DonutChartModule } from 'projects/donut-chart/src/public_api';
-import { DonutChartModule } from '../../dist/donut-chart';
+// import { DonutChartModule } from '../../dist/donut-chart';
+import { DonutChartModule } from 'donut-chart';
 
 
 @NgModule({
